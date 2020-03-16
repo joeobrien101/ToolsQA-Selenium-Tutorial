e3418170835b879045a6bea7b9159227dc821d5f
joeobrien101 <jjob.obrien97@gmail.com>
2020-03-16 10:49:56 +0000

    Added FindElement + Checkbox commands

---
+++ Selenium Basics/FindElementCommands.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+namespace ToolsQA.Selenium_Basics
+{
+ 
+    class FindElementCommands
+    {
+
+        [Test]
+        public void Test()
+        {
+            IWebDriver driver = new FirefoxDriver(@"F:/Selenium");
+
+            driver.Url = "http://toolsqa.com/automation-practice-form/";
+
+            // Type Name in the FirstName text box
+            driver.FindElement(By.Name("firstname")).SendKeys("Joe");
+
+            //Type LastName in the LastName text box
+            driver.FindElement(By.Name("lastname")).SendKeys("O'Brien");
+
+            // Click on the Submit button
+            driver.FindElement(By.Id("submit")).Click();
+        }
+    }
+}
---
+++ Selenium Basics/FindElementCommands_2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+namespace ToolsQA.Selenium_Basics
+{
+    class FindElementCommands_2
+    {
+
+        [Test]
+        public void Test()
+        {
+            // Create a new instance of the FireFox driver
+            IWebDriver driver = new FirefoxDriver();
+
+            // Launch the Online Store WebSite
+            driver.Url = ("http://toolsqa.com/Automation-practice-form/");
+
+            // Click on "Partial Link Text" link
+            driver.FindElement(By.PartialLinkText("Partial")).Click();
+            Console.WriteLine("Partial Link Test Pass");
+
+            // Convert element in to a string 
+            String sClass = driver.FindElements(By.TagName("button")).ToString();
+            Console.WriteLine(sClass);
+
+            // Click on "Link Text" link
+            driver.FindElement(By.LinkText("Link Test")).Click();
+            Console.WriteLine("Link Test Pass");
+        }
+    }
+}
